5a19e25ae5b38560b1e789ff406baedfa1343633
nikitha262 <87030712+nikitha262@users.noreply.github.com>
2021-07-10 21:29:05 +0530

    Add files via upload

---
+++ Assignment2/Fizz Buzz.js
@@ -0,0 +1,20 @@
+for(i=1; i<=200; i++)
+{
+
+    if (i%20==0)
+    {
+        console.log("FizzBuzz")
+    }
+    else if (i%2==0)
+    {
+        console.log("Fizz")
+    }
+    else if (i%4==0)
+    {
+        console.log("Buzz")
+    }
+    else
+    {
+        console.log(i)
+    }
+}
---
+++ Assignment2/Palindrome string.js
@@ -0,0 +1,17 @@
+//palindromes
+
+s = "india won the match yesterday"
+s1 = " "
+s  = s + " "
+    for (t of s) {
+        if (t != " " ) {
+            s1 = s1 + t
+            
+        }
+    
+        else{
+        console.log(s1)
+        word=(" ")
+        }
+    }
+    
